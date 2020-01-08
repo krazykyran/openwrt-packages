01a15698970260ef4b49a321290234a345f8144f
Simon Crawley <simon.p.crawley@gmail.com>
2020-01-09 10:21:13 +1030

    luci-app-edge: change file structure

--- luci/applications/luci-app-edge/luasrc/controller/admin/edge.lua
+++ luci/applications/luci-app-edge/luasrc/controller/admin/edge.lua
@@ -1,7 +1,7 @@
 -- Copyright 2020 Simon Crawley <simon.crawley@gotosage.com>
 -- Licensed to the public under the Apache License 2.0.
 
-module("luci.controller.edge", package.seeall)
+module("luci.controller.admin.edge", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/edge") then
@@ -10,6 +10,6 @@ function index()
 	
 	local page
 	
-	page = entry({"admin", "system", "edge"}, cbi("edge"), _("EDGE Configuration"))
+	page = entry({"admin", "edge", "config"}, cbi("admin_edge/config"), _("Configuration"))
 	page.dependent = true
 end
