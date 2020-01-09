517651d4a679f3338c3bcbc2a42cd0b295de705f
Simon Crawley <simon.p.crawley@gmail.com>
2020-01-09 10:45:46 +1030

    luci-mod-edge: change application to module

--- luci/modules/luci-mod-edge/luasrc/controller/edge.lua
+++ luci/modules/luci-mod-edge/luasrc/controller/edge.lua
@@ -1,15 +1,12 @@
 -- Copyright 2020 Simon Crawley <simon.crawley@gotosage.com>
 -- Licensed to the public under the Apache License 2.0.
 
-module("luci.controller.admin.edge", package.seeall)
+module("luci.controller.edge", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/edge") then
 		return
 	end
 	
-	local page
-	
-	page = entry({"admin", "edge", "config"}, cbi("admin_edge/config"), _("Configuration"))
-	page.dependent = true
+	entry({"admin", "edge", "config"}, cbi("edge/config"), _("Configuration")).dependent=true
 end
